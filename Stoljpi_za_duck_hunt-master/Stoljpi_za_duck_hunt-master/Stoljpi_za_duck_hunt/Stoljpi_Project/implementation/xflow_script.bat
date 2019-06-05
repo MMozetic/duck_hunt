0cd37a791f2edef5e5b311cfe5280341bcd8fbd5
Petar <nspavle@gmail.com>
2019-06-05 09:26:35 +0200

    sad radi

--- Stoljpi_za_duck_hunt-master/Stoljpi_za_duck_hunt-master/Stoljpi_za_duck_hunt/Stoljpi_Project/implementation/xflow_script.bat
+++ Stoljpi_za_duck_hunt-master/Stoljpi_za_duck_hunt-master/Stoljpi_za_duck_hunt/Stoljpi_Project/implementation/xflow_script.bat
@@ -6,7 +6,7 @@
 @REM #
 @REM # Command line for ngdbuild
 @REM #
-ngdbuild -p xc6slx45fgg676-2 -nt timestamp -bm system.bmm "C:/Users/student/Downloads/Stoljpi_za_duck_hunt-master/Stoljpi_za_duck_hunt-master/Stoljpi_za_duck_hunt/Stoljpi_Project/implementation/system.ngc" -uc system.ucf system.ngd 
+ngdbuild -p xc6slx45fgg676-2 -nt timestamp -bm system.bmm "D:/Patka/duck_hunt/Stoljpi_za_duck_hunt-master/Stoljpi_za_duck_hunt-master/Stoljpi_za_duck_hunt/Stoljpi_Project/implementation/system.ngc" -uc system.ucf system.ngd 
 
 @REM #
 @REM # Command line for map
